7ab2d31910db8c8774ecd34b9f8c15c6f8273634
maha507 <132257569+maha507@users.noreply.github.com>
2023-06-06 14:29:52 +0530

    add

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { onMount } from 'svelte';
-	import { Grid } from 'gridjs-selte';
+	import { Grid } from 'gridjs';
 	import 'bootstrap/dist/css/bootstrap.min.css';
 	let grid;
 	let jsonData = [];
