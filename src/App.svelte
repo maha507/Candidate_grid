57d6d882c8ccbc1996b52f7f1856de7dfc43f5e3
maha507 <132257569+maha507@users.noreply.github.com>
2023-06-07 14:55:41 +0530

    grid

--- src/App.svelte
+++ src/App.svelte
@@ -1,78 +1,46 @@
 <script>
-
-  import { onMount } from "svelte";
-
-  import Grid from "gridjs-svelte";
-  let jsonData = [];
-  onMount(async () => {
-
-    await fetchData();
-
-  });
-
-  async function fetchData() {
-
-    const response = await fetch("https://api.recruitly.io/api/jobapiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3");
-
-    const responseData = await response.json();
-
-    jsonData = responseData.data;
-
-  }
-
-  function titleFormatter(cell) {
-
-    return '<a href="#">${cell}</a>';
-
-  }
-
-</script>
-<main>
-
-  <Grid
-
-    search
-
-    sort
-
-    pagination={{ enabled: true, limit: 10 }}
-
-    data={jsonData}
-
-    columns={[
-
-      {
-
-        name: "Title",
-
-        formatter: titleFormatter
-
-      },
-
-      {
-
-        name: "Reference",
-
-        selector: "reference"
-
-      },
-
-      {
-
-        name: "Status",
-
-        selector: "status"
-
-      }
-
-    ]} />
-
-</main>
-
-
-
-<style global>
-
-  @import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
-
-</style>
+	import { onMount } from "svelte";
+	import Grid from "gridjs-svelte";
+  
+	let jsonData = [];
+	let tableVisible = false;
+  
+	onMount(async () => {
+	  const response = await fetch("https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3");
+	  const responseData = await response.json();
+	  jsonData = responseData.data;
+	});
+  
+	function openTable() {
+	  if (!tableVisible) {
+		fetchData();
+	  }
+	}
+  
+	async function fetchData() {
+	  const response = await fetch("https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3");
+	  const responseData = await response.json();
+	  jsonData = responseData.data;
+	  tableVisible = true;
+	}
+  </script>
+  
+  <button on:click={openTable}>Display Table</button>
+  
+  <main>
+	{#if tableVisible}
+	  <Grid
+		search
+		sort
+		pagination={{ enabled: true, limit: 38 }}
+		data={jsonData.map(item => ({
+		  title: item.title,
+		  reference: item.reference,
+		  status: item.status
+		}))} />
+	{/if}
+  </main>
+  
+  <style global>
+	@import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
+  </style>
