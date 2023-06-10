3f823210a6fc02ef5f620a419f36af282ce6cff8
maha507 <132257569+maha507@users.noreply.github.com>
2023-06-10 14:25:30 +0530

    grid

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,27 @@
 <script>
 
   import { onMount } from "svelte";
-
+  import 'bootstrap/dist/css/bootstrap.min.css';
   // Sample data
 
   let jsonData = [];
-
   let data = [];
 
   onMount(async () => {
-
       const response = await fetch(
-
           "https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154"
-
       );
-
       const responseData = await response.json();
-
       jsonData = responseData.data;
-
       console.log(jsonData, "json");
-
       const gridData = jsonData.map(item => ({
 
   //   id: item.id,
 
   reference:item.reference,
-
     name: item.fullName,
-
     email:item.email,
-
     phone:item.mobile,
-
     // map other properties accordingly
 
   }));
@@ -41,109 +29,50 @@
 console.log(gridData,"griddata");
 
   // });
-
-
-
-
   var dataGrid = new DevExpress.ui.dxDataGrid("#dataGrid", {
-
       dataSource: gridData,
-
       columns: [
-
       { dataField: 'reference', caption: 'ID' },
-
       { dataField: 'name', caption: 'Name', dataType: "url" },
-
       { dataField: 'email', caption: 'Email' },
-
       { dataField: 'phone', caption: 'Mobile' },
-
       // Define other columns as needed
-
     ],
-
-
-
-
-      
-
       showBorders: true,
-
       filterRow: {
-
           visible: true,
-
       },
-
       // editing: {
-
-          
-
       //  allowUpdating: true,
-
       //  allowDeleting: true,
-
       //  allowAdding: true,
-
       // },
-
-
-
-
-
       editing: {
-
           allowDeleting: true,
-
           allowAdding: true,
-
           allowUpdating: true,
-
           mode: "popup",
-
           form: {
-
               labelLocation: "top"
-
           },
-
           popup: {
-
               showTitle: true,
-
               title: "Row in the editing state"
-
           }
-
       },
-
       paging: {
-
           pageSize: 10,
-
       },
-
       // pagination:true,
-
       pager: {
-
           showPageSizeSelector: true,
-
           allowedPageSizes: [5, 10, 20],
-
           showInfo: true,
-
       },
-
   });
-
   });
-
   // dataGrid.render();
-
   // });
-
 </script>
 
 <div id="dataGrid"></div>
