0f422cab2512ada9e22fff8cb0d101402de852d6
maha507 <132257569+maha507@users.noreply.github.com>
2023-06-10 14:41:49 +0530

    grid

--- src/App.svelte
+++ src/App.svelte
@@ -1,74 +1,58 @@
 <script>
-
-  import { onMount } from "svelte";
-  import 'bootstrap/dist/css/bootstrap.min.css';
-  // Sample data
-
-  let jsonData = [];
-  let data = [];
-
-  onMount(async () => {
+    import { onMount } from "svelte";
+    import "bootstrap/dist/css/bootstrap.min.css";
+    import DevExpress from "devextreme";
+  
+    let jsonData = [];
+    let gridData = [];
+  
+    onMount(async () => {
       const response = await fetch(
-          "https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154"
+        "https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154"
       );
       const responseData = await response.json();
       jsonData = responseData.data;
-      console.log(jsonData, "json");
-      const gridData = jsonData.map(item => ({
-
-  //   id: item.id,
-
-
-    name: item.fullName,
-    email:item.email,
-    phone:item.mobile,
-    // map other properties accordingly
-
-  }));
-
-console.log(gridData,"griddata");
-
-  // });
-  var dataGrid = new DevExpress.ui.dxDataGrid("#dataGrid", {
-      dataSource: gridData,
-      columns: [
-     
-      { dataField: 'name', caption: 'Name', dataType: "url" },
-      { dataField: 'email', caption: 'Email' },
-      { dataField: 'phone', caption: 'Mobile' },
-      // Define other columns as needed
-    ],
-      showBorders: true,
-      filterRow: {
+  
+      gridData = jsonData.map((item) => ({
+        firstname: item.firstname,
+        surname: item.surname,
+        email: item.email,
+        phone: item.mobile,
+      }));
+  
+      const dataGrid = new DevExpress.ui.dxDataGrid(document.getElementById("dataGrid"), {
+        dataSource: gridData,
+        columns: [
+          { dataField: "firstname", caption: "First Name" },
+          { dataField: "surname", caption: "Last Name" },
+          { dataField: "email", caption: "Email" },
+          { dataField: "phone", caption: "Mobile" },
+        ],
+        showBorders: true,
+        filterRow: {
           visible: true,
-      },
-      // editing: {
-      //  allowUpdating: true,
-      //  allowDeleting: true,
-      //  allowAdding: true,
-      // },
-      editing: {
+        },
+        editing: {
           allowDeleting: true,
           allowAdding: true,
           allowUpdating: true,
           mode: "popup",
           form: {
-              labelLocation: "top"
+            labelLocation: "top",
           },
           popup: {
-              showTitle: true,
-              title: "Row in the editing state"
-          }
-      },
-      paging: {
+            showTitle: true,
+            title: "Row in the editing state",
+          },
+        },
+        paging: {
           pageSize: 10,
-      },
-      // pagination:true,
-     
-  });
-  });
-  // dataGrid.render();
-  // });
-</script>
-
-<div id="dataGrid"></div>
+        },
+      });
+  
+      dataGrid.render();
+    });
+  </script>
+  
+  <div id="dataGrid"></div>
+  
